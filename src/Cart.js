fedfd628443c0022111e61fb9fcca093909012fe
shweta1907 <shweta1907sharma@gmail.com>
2020-09-16 23:33:47 +0530

    added props

--- src/Cart.js
+++ src/Cart.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import CartItem from './CartItem'
 class Cart extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+        products:[
+            {
+                price:99,
+                title:'watch',
+                qty:1,
+                img:'',
+                id:1
+            },
+            {
+                price:109,
+                title:'phone',
+                qty:1,
+                img:'',
+                id:2
+            },
+            {
+                price:99999,
+                title:'bts album',
+                qty:1,
+                img:'',
+                id:3
+            }
+        ]
+        }
+      }
     render () {
+        const {products}=this.state;
       return (
         <div className="cart">
-          <CartItem />
-          <CartItem />
-          <CartItem />
+                {
+                    products.map((product)=>{
+                        return <CartItem product={product} key={product.id}/>
+                    })
+                }
         </div>
       );
     }
